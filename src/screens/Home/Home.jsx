bc8769511acf9d0f503193e3e17e395e94ef8adc
jim-les <jimleston35@gmail.com>
2024-04-04 10:58:56 +0300

    Update the home page to redirect user to landing pqagge on first visit

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Link, Navigate } from 'react-router-dom';
 import Topbar from './Components/Topbar';
 import HomeCardOne from './Components/HomeCardOne';
-import "./Home.css"
-// icons
+import BottomNav from '../../Components/BottomNav';
 import zantrikIcon from '../../assets/car.png';
 import repairIcon from '../../assets/car-repair.png';
 import car from '../../assets/car.png';
@@ -11,11 +11,19 @@ import digitalDriver from "../../assets/driver (1).png";
 import driver from '../../assets/driver.png';
 import sellCrarIcon from '../../assets/car-money.png';
 import { AddTask } from '@mui/icons-material';
-import BottomNav from '../../Components/BottomNav';
-import { Link, useNavigate } from 'react-router-dom';
 
 export default function Home() {
-    const navigate = useNavigate();
+    const [firstVisit, setFirstVisit] = useState(true);
+
+    useEffect(() => {
+        // Set firstVisit to false after the first render
+        setFirstVisit(false);
+    }, []);
+
+    if (firstVisit) {
+        // Redirect to landing page on first visit
+        return <Navigate to="/landing" />;
+    }
 
     return (
         <div className='home-screen'>
